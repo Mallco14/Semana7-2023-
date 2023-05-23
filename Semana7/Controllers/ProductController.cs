d7275fcf7622aecc1e5e4ed8f740d148725b3bfa
Alumno <Alumno@LAB707-09>
2023-05-22 21:30:11 -0500

    Agregar archivos de proyecto.

---
+++ Semana7/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using Semana7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana7.Controllers
+{
+    public class ProductController : Controller
+    {
+        //crear objeto de la base de datos
+        private NeptunoEntities db = new NeptunoEntities();        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombreProducto,precioUnidad,categoriaProducto")] productos producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Semana7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Semana7.MvcApplication" Language="C#" %>
